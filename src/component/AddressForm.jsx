11db0b283f3f499d90b25693e79ba9133a32fffd
Sarah Adamson <syadamso@ucsc.edu>
2020-11-05 11:09:46 -0800

    updated entry names, and is now mobile friendly

---
+++ src/component/AddressForm.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import './AddressForm.scss'
+class AddressForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value1: "", value2: "" };
+
+    this.handleChange1 = this.handleChange1.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange1(event) {
+    this.setState({ value1: event.target.value, value2: this.state.value2 });
+  }
+
+  handleChange2(event) {
+    this.setState({ value1: this.state.value1, value2: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    document.getElementById("output").innerHTML =
+      "Your favorite flavor is: " +
+      this.state.value1 +
+      "\nand\n" +
+      this.state.value2;
+  }
+
+  get addresses() {
+    return this.state;
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label class ="FromLabel">
+          Enter Pick-up Location:
+          <input
+            id="FromInput"
+            type="text"
+            value={this.state.value1}
+            onChange={this.handleChange1}
+          />
+        </label>
+        <label class = "ToLabel">
+          Enter Destination:
+          <input
+            id="ToInput"
+            type="text"
+            value={this.state.value2}
+            onChange={this.handleChange2}
+          />
+        </label>
+        <input id="SubmitLabel" type="submit" value="COMPARE!" />
+        <p id="output"></p>
+      </form>
+    );
+  }
+}
+
+export default AddressForm;
--- src/component/landing/index.jsx
+++ src/component/landing/index.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import Address from "../Address";
 import "./index.scss";
-
+import AddressForm from "../AddressForm";
 const LandingPageView = () => {
   return (
     <div className="LandingPage">
       <h1>Service Price Comparer</h1>
-      <div className ="Tab1">Rideshare</div>
-      <div className = "Tab2">Food Delivery</div>
-      <Address text="Enter Pickup Location:" render={() => <Address />} />
-      <Address text="Enter Destination:" render={() => <Address />} />
-      <button class="submitButton" type="button">
-        Submit
-      </button>
+      <AddressForm render={() => <AddressForm />} />
     </div>
   );
 };
