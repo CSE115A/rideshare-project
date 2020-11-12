31b7ab2bde54c86bad1abb76acd9084420885e11
Isai Lopez <ilopezro@ucsc.edu>
2020-11-12 14:19:51 -0800

    refactored code allowing landing page to control state

--- src/component/landing/index.jsx
+++ src/component/landing/index.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import AddressForm from "../AddressForm/AddressForm";
 
 const LandingPageView = () => {
+  const [originAddress, changeOriginAddress] = useState("");
+  const [destinationAddress, changeDestinationAddress] = useState("");
+
   return (
     <div className="LandingPage">
       <h1>Service Price Comparer</h1>
-      <AddressForm />
+      <AddressForm
+        originAddress={originAddress}
+        destinationAddress={destinationAddress}
+        changeOriginAddress={changeOriginAddress}
+        changeDestinationAddress={changeDestinationAddress}
+      />
     </div>
   );
 };
