847cc6adc8efcf3d0c1837fe39e082a46b2fb837
Isai Lopez <ilopezro@ucsc.edu>
2020-11-15 18:46:12 -0800

    changing to flex

--- src/component/AddressForm/AddressForm.jsx
+++ src/component/AddressForm/AddressForm.jsx
@@ -14,57 +14,53 @@ const AddressForm = ({
   return (
     <div className="AddressForm">
       <div className="AddressForm__originContainer">
-        <label className="AddressForm__originLabel">
-          Pick-Up Location:
-          <input
-            id="FromInput"
-            type="text"
-            className="AddressForm__originInput"
-            placeholder="Enter Pickup Location"
-            value={originAddress}
-            onChange={async (e) => {
-              changeOriginAddress(e.target.value);
-              changeOriginSuggestions(
-                await getPredictions({ input: e.target.value })
-              );
-            }}
-          />
-          {originSuggestions !== undefined &&
-            originSuggestions.map(({ description }) => (
-              <li
-                key={description}
-                onClick={() => changeOriginAddress(description)}
-              >
-                {description}
-              </li>
-            ))}
-        </label>
+        <div className="AddressForm__originLabel">Pick-Up Location:</div>
+        <input
+          id="FromInput"
+          type="text"
+          className="AddressForm__originInput"
+          placeholder="Enter Pickup Location"
+          value={originAddress}
+          onChange={async (e) => {
+            changeOriginAddress(e.target.value);
+            changeOriginSuggestions(
+              await getPredictions({ input: e.target.value })
+            );
+          }}
+        />
+        {originSuggestions !== undefined &&
+          originSuggestions.map(({ description }) => (
+            <li
+              key={description}
+              onClick={() => changeOriginAddress(description)}
+            >
+              {description}
+            </li>
+          ))}
       </div>
       <div className="AddressForm__destinationContainer">
-        <label className="AddressForm__destinationContainer">
-          Drop Off Location:
-          <input
-            id="ToInput"
-            type="text"
-            placeholder="Enter Dropoff Location"
-            value={destinationAddress}
-            onChange={async (e) => {
-              changeDestinationAddress(e.target.value);
-              changeDestinationSuggestions(
-                await getPredictions({ input: e.target.value })
-              );
-            }}
-          />
-          {destinationSuggestions !== undefined &&
-            destinationSuggestions.map(({ description }) => (
-              <li
-                key={description}
-                onClick={() => changeDestinationAddress(description)}
-              >
-                {description}
-              </li>
-            ))}
-        </label>
+        <div className="AddressForm__destinationLabel">Drop Off Location:</div>
+        <input
+          id="ToInput"
+          type="text"
+          placeholder="Enter Dropoff Location"
+          value={destinationAddress}
+          onChange={async (e) => {
+            changeDestinationAddress(e.target.value);
+            changeDestinationSuggestions(
+              await getPredictions({ input: e.target.value })
+            );
+          }}
+        />
+        {destinationSuggestions !== undefined &&
+          destinationSuggestions.map(({ description }) => (
+            <li
+              key={description}
+              onClick={() => changeDestinationAddress(description)}
+            >
+              {description}
+            </li>
+          ))}
       </div>
     </div>
   );
