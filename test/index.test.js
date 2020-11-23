23ae8e46575596dcfaba238f6db205c464aa4a9a
Prateek Narayanan <prateeknarayanan@gmail.com>
2020-11-22 23:24:09 -0800

    Created test case for submitting form

--- test/index.test.js
+++ test/index.test.js
@@ -10,12 +10,23 @@ afterAll(async () => {
   await browser.close();
 });
 
-describe("First Test", () => {
+describe("Map Test", () => {
   test("GET Map element", async () => {
     const page = await browser.newPage();
     await page.goto(`http://localhost:3000`);
     await page.waitForSelector(".Map");
     const element = await page.$(".Map");
     expect(element).toBeDefined();
+  });
+});
+
+describe("Submit Test", () => {
+  test("Submitting form", async () => {
+    const page = await browser.newPage();
+    await page.goto(`http://localhost:3000`);
+    console.log(page.url());
+    await page.type();
+    await page.type();
+    await page.click('Compare!');
   });
 });
