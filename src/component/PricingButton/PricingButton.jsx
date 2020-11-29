d1f401cdb8915ef7bfb962eb1c3fa75582340dab
Ernesto <eherna65@ucsc.edu>
2020-11-28 17:38:18 -0800

    Button Styling

--- src/component/PricingButton/PricingButton.jsx
+++ src/component/PricingButton/PricingButton.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import axios from "axios";
 import "./index.scss";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 const authToken = process.env.REACT_APP_PRICES_AUTH_TOKEN;
 const pricesEndpoint = process.env.REACT_APP_PRICES_ENDPOINT;
+const useStyles = makeStyles({
+  PricingButton__button: {
+    background: "linear-gradient(45deg, #0BD989 30%, #0BD989 99%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+  },
+});
 
 async function getPrices({ changeDisplayPricing }) {
   const params = {
@@ -29,14 +42,15 @@ async function getPrices({ changeDisplayPricing }) {
 }
 
 const PricingButton = ({ changeDisplayPricing }) => {
+  const classes = useStyles();
   return (
     <div className="PricingButton">
-      <button
-        className="PricingButton__button"
+      <Button
+        className={classes.PricingButton__button}
         onClick={() => getPrices({ changeDisplayPricing })}
       >
         Compare!
-      </button>
+      </Button>
     </div>
   );
 };
