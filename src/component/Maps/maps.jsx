b4ae70c88f9632ae699fa50f639fa6d8bea8244d
Sarah Adamson <syadamso@ucsc.edu>
2020-11-29 16:00:12 -0800

    location pins working

--- src/component/Maps/maps.jsx
+++ src/component/Maps/maps.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./maps.scss";
 import GoogleMapReact from "google-map-react";
 
+
 import { Icon } from "@iconify/react";
 import locationIcon from "@iconify/icons-mdi/map-marker";
 import Polyline from "google-map-react";
@@ -10,15 +11,15 @@ const google_maps_key = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 const Location1 = ({ text1 }) => (
   <div className="LocationPin">
     <Icon icon={locationIcon} className="LocationPin__icon" />
-    <p className="LocationPin__text">{text1}</p>
   </div>
 );
 const Location2 = ({ text2 }) => (
   <div className="LocationPin">
     <Icon icon={locationIcon} className="LocationPin__icon" />
-    <p className="LocationPin__text">{text2}</p>
   </div>
 );
+const Marker = ({ text }) => <div>{text}</div>;
+
 
 
 //Map component that takes in a location and zoom level
@@ -30,23 +31,30 @@ const Map = ({pathCoords, startLocation, endLocation, zoomLevel }) => {
         defaultCenter={startLocation}
         defaultZoom={zoomLevel}
       >
-        <Location1
+        <Marker
+        lat={startLocation.lat}
+        lng={startLocation.lng}
+        text ="Marker"
+        />
+
+         <Location1
           lat={startLocation.lat}
           lng={startLocation.lng}
           text1={startLocation.address}
-        />
+        /> 
+
         <Location2
           lat={endLocation.lat}
           lng={endLocation.lng}
           text2={endLocation.address}
-        />
-        <Polyline 
+        /> 
+        {/* <Polyline 
         path = {pathCoords}
         geodesic = {true}
         strokeColor = {'#669DF6'}
         strokeOpacity = {1.0}
         strokeWeight = {2}
-        />
+        /> */}
       </GoogleMapReact>
     </div>
   );
