74f8a59cd2d1a2fc4f7041331f2f2b0b2a058d7d
Isai Lopez <ilopezro@ucsc.edu>
2020-11-29 22:16:57 -0800

    converted prices into an object to delete duplicate code

--- src/component/PricesOutput/index.jsx
+++ src/component/PricesOutput/index.jsx
@@ -2,24 +2,20 @@ import React from "react";
 import "./index.scss";
 
 const PricesOutput = ({ lyftPrices, uberPrices }) => {
+  const pricesObject = [lyftPrices, uberPrices];
+
   return (
     <div className="PricesOutput">
-      <div className="PricesOutput__pricesContainer">
-        {lyftPrices.map(({ displayName, price }) => (
-          <div key={displayName} className="PricesOutput__priceItemContainer">
-            <span className="PricesOutput__pricesItem">{displayName}</span>
-            <span className="PricesOutput__pricesItem">{price}</span>
-          </div>
-        ))}
-      </div>
-      <div className="PricesOutput__pricesContainer">
-        {uberPrices.map(({ displayName, price }) => (
-          <div key={displayName} className="PricesOutput__priceItemContainer">
-            <span className="PricesOutput__pricesItem">{displayName}</span>
-            <span className="PricesOutput__pricesItem">{price}</span>
-          </div>
-        ))}
-      </div>
+      {pricesObject.map((ridesharePrices) => (
+        <div className="PricesOutput__pricesContainer">
+          {ridesharePrices.map(({ displayName, price }) => (
+            <div key={displayName} className="PricesOutput__priceItemContainer">
+              <span className="PricesOutput__pricesItem">{displayName}</span>
+              <span className="PricesOutput__pricesItem">{price}</span>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
