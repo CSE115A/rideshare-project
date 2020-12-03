eeb0bf91714b0c429b01df94c59d612f01cb0e2e
dunchris <dungochris@gmail.com>
2020-12-03 10:38:02 -0800

    fixed the map pins size

--- src/component/PricesOutput/index.jsx
+++ src/component/PricesOutput/index.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import "./index.scss";
 
 const PricesOutput = ({ lyftPrices, uberPrices }) => {
-  const pricesObject = [lyftPrices, uberPrices];
+  const pricesObject = [ { columnName: "Lyft Prices", prices: lyftPrices }, { columnName: "Uber Prices", prices: uberPrices }];
 
   return (
     <div className="PricesOutput">
       {pricesObject.map((ridesharePrices) => (
         <div className="PricesOutput__pricesContainer">
-          {ridesharePrices.map(({ displayName, price }) => (
+          <span className="PricesOutput__header">{ridesharePrices.columnName}</span>
+          {ridesharePrices.prices.map(({ displayName, price }) => (
             <div key={displayName} className="PricesOutput__priceItemContainer">
               <span className="PricesOutput__pricesItem">{displayName}</span>
-              <span className="PricesOutput__pricesItem">{price}</span>
+              <span className="PricesOutput__pricesItem">{price ? price : "Not Available"}</span>
             </div>
           ))}
         </div>
