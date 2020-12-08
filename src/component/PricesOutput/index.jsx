56c58f49e47fd60095379f4204d1159cfb3ed67f
ilopezro <ilopezro@ucsc.edu>
2020-12-07 21:24:02 -0800

    cleaned up prices output file

--- src/component/PricesOutput/index.jsx
+++ src/component/PricesOutput/index.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import "./index.scss";
 
 const PricesOutput = ({ lyftPrices, uberPrices }) => {
-  const pricesObject = [
-    { columnName: "Lyft Prices", prices: lyftPrices },
-    { columnName: "Uber Prices", prices: uberPrices },
-  ];
+  const pricesObject = {
+    "Lyft Prices": lyftPrices,
+    "Uber Prices": uberPrices,
+  };
+
+  console.log(pricesObject);
 
   return (
     <div className="PricesOutput">
-      {pricesObject.map((ridesharePrices) => (
+      {Object.entries(pricesObject).map(([type, prices]) => (
         <div className="PricesOutput__pricesContainer">
-          <span className="PricesOutput__header">
-            {ridesharePrices.columnName}
-          </span>
-          {ridesharePrices.prices.map(({ displayName, price }) => (
+          <span className="PricesOutput__header">{type}</span>
+          {prices.map(({ displayName, price }) => (
             <div key={displayName} className="PricesOutput__priceItemContainer">
               <span className="PricesOutput__pricesItem">{displayName}</span>
               <span className="PricesOutput__pricesItem">
