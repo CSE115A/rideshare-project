fee57e711f70e1036e6f93a02c06a0e7d094851c
Ernesto <eherna65@ucsc.edu>
2020-12-08 14:09:29 -0800

    white space

--- src/component/AppBar/AppBar.jsx
+++ src/component/AppBar/AppBar.jsx
@@ -6,7 +6,7 @@ import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    background: "linear-gradient(45deg, #5dc9d9 30%, #0BD989 99%)",
+    background: "linear-gradient(45deg, #5dc9d9 30%, #0bd989 99%)",
   },
   menuButton: {
     marginRight: theme.spacing(2),
