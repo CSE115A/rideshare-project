841c429dda9033df1dbaf0d850348b3298f26d23
ilopezro <ilopezro@ucsc.edu>
2020-12-07 20:57:08 -0800

    added styling to test jest index page

--- test/index.test.js
+++ test/index.test.js
@@ -1,52 +1,53 @@
+/* eslint-disable jest/no-disabled-tests */
 const puppeteer = require("puppeteer");
 
 let browser;
 
 beforeAll(async () => {
-  browser = await puppeteer.launch({})
+  browser = await puppeteer.launch({});
 });
 
 let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto(`http://localhost:3000`)
-  await page.waitForSelector(".LandingPage")
+  await page.goto(`http://localhost:3000`);
+  await page.waitForSelector(".LandingPage");
 });
 
 afterAll(async () => {
-  await browser.close()
+  await browser.close();
 });
-
 
 describe("Loading Components Tests", () => {
   test("GET Map element", async () => {
-    await page.waitForSelector(".Map")
-    const map = await page.$(".Map")
-    expect(map).toBeDefined()
+    await page.waitForSelector(".Map");
+    const map = await page.$(".Map");
+    expect(map).toBeDefined();
   });
 
   test("Location pin and its address shows up when an address is entered and cleared when address is cleared", async () => {
     const form_boxes = await page.$$("input");
-    for (const box of form_boxes)
-    {
-      await box.click()
-      await box.type("112")
-      await page.waitForSelector("div[id$=option-0]")
+    for (const box of form_boxes) {
+      await box.click();
+      await box.type("112");
+      await page.waitForSelector("div[id$=option-0]");
 
-      const first_option = await page.$("div[id$=option-0]")
-      const option_val = await first_option.evaluate((el) => el.innerText)
-      await first_option.click()
-      
-      await page.waitForSelector(".LocationPin")
-      const location = await page.$(".LocationPin")
-      expect(location).not.toBe(null)
-      const location_text = await page.$eval(".LocationPin__text", (el) => el.innerText)
-      expect(location_text).toBeDefined()
-      await box.click()
-      await page.keyboard.press("Delete")
+      const first_option = await page.$("div[id$=option-0]");
+      const option_val = await first_option.evaluate((el) => el.innerText);
+      await first_option.click();
+
+      await page.waitForSelector(".LocationPin");
+      const location = await page.$(".LocationPin");
+      expect(location).not.toBe(null);
+      const location_text = await page.$eval(
+        ".LocationPin__text",
+        (el) => el.innerText
+      );
+      expect(location_text).toBeDefined();
+      await box.click();
+      await page.keyboard.press("Delete");
     }
-    
   });
 
   test("Two location pins show up when both addresses are entered", async () => {
@@ -54,21 +55,19 @@ describe("Loading Components Tests", () => {
     const pickup_box = form_boxes[0];
     const dropoff_box = form_boxes[1];
 
-    box_inputs = ["San Francisco", "San Jose"]
-    for (let id = 0; id < form_boxes.length; id++)
-    {
-      await form_boxes[id].click()
-      await form_boxes[id].type(box_inputs[id])
-      await page.waitForSelector("div[id$=option-0]")
+    box_inputs = ["San Francisco", "San Jose"];
+    for (let id = 0; id < form_boxes.length; id++) {
+      await form_boxes[id].click();
+      await form_boxes[id].type(box_inputs[id]);
+      await page.waitForSelector("div[id$=option-0]");
 
-      let first_option = await page.$("div[id$=option-0]")
-      await first_option.click()
+      let first_option = await page.$("div[id$=option-0]");
+      await first_option.click();
     }
-    await page.waitForTimeout(700)
-    const locations = await page.$$(".LocationPin")
-    expect(locations.length).toBe(2)
-    
-  })
+    await page.waitForTimeout(700);
+    const locations = await page.$$(".LocationPin");
+    expect(locations.length).toBe(2);
+  });
 });
 
 describe("Components Test", () => {
@@ -89,14 +88,12 @@ describe("Components Test", () => {
   });
 
   test("Uber and Lyft Price Lists do not appear on loading", async () => {
-    const prices = await page.$(".PricesOutput")
-    expect(prices).toBe(null)
-  })
-
+    const prices = await page.$(".PricesOutput");
+    expect(prices).toBe(null);
+  });
 });
 
 describe("Address Form Input tests", () => {
-
   test("clicking off of a box after typing in it will clear it", async () => {
     const form_boxes = await page.$$("input");
     const pickup_box = form_boxes[0];
@@ -129,56 +126,54 @@ describe("Address Form Input tests", () => {
   });
 
   test("a selected address from the drop down will be displayed in the text box", async () => {
- 
     const form_boxes = await page.$$("input");
-    for (const box of form_boxes)
-    {
+    for (const box of form_boxes) {
       await box.click();
       await box.type("112");
-      await page.waitForSelector("div[id$=option-0]")
+      await page.waitForSelector("div[id$=option-0]");
 
-      const first_option = await page.$("div[id$=option-0]")
-      await first_option.click()
-      const option_val = await first_option.evaluate((el) => el.innerText)
-      
-      await page.waitForSelector("div[class$=singleValue]")
-      let pickupVal = await page.$eval("div[class$=singleValue]", (el) => el.innerText);
-      
-      expect(pickupVal).toEqual(option_val)
+      const first_option = await page.$("div[id$=option-0]");
+      await first_option.click();
+      const option_val = await first_option.evaluate((el) => el.innerText);
+
+      await page.waitForSelector("div[class$=singleValue]");
+      let pickupVal = await page.$eval(
+        "div[class$=singleValue]",
+        (el) => el.innerText
+      );
+
+      expect(pickupVal).toEqual(option_val);
     }
-    
   });
-
 });
 
 describe("Map Tests", () => {
-  
   test("Both the location pin and its address shows up when an address is entered and cleared when address is cleared", async () => {
     const form_boxes = await page.$$("input");
-    for (const box of form_boxes)
-    {
-      await box.click()
-      await box.type("112")
-      await page.waitForSelector("div[id$=option-0]")
+    for (const box of form_boxes) {
+      await box.click();
+      await box.type("112");
+      await page.waitForSelector("div[id$=option-0]");
 
-      const first_option = await page.$("div[id$=option-0]")
-      const option_val = await first_option.evaluate((el) => el.innerText)
-      await first_option.click()
-      
-      await page.waitForSelector(".LocationPin")
-      let location = await page.$(".LocationPin")
-      expect(location).not.toBe(null)
-      let location_text = await page.$eval(".LocationPin__text", (el) => el.innerText)
-      expect(location_text).toBeDefined()
-      
-      await box.click()
-      await page.keyboard.press("Delete")
-      await page.waitForSelector(".LocationPin", {hidden: true})
-      location = await page.$(".LocationPin")
-      expect(location).toBe(null)
+      const first_option = await page.$("div[id$=option-0]");
+      const option_val = await first_option.evaluate((el) => el.innerText);
+      await first_option.click();
 
+      await page.waitForSelector(".LocationPin");
+      let location = await page.$(".LocationPin");
+      expect(location).not.toBe(null);
+      let location_text = await page.$eval(
+        ".LocationPin__text",
+        (el) => el.innerText
+      );
+      expect(location_text).toBeDefined();
+
+      await box.click();
+      await page.keyboard.press("Delete");
+      await page.waitForSelector(".LocationPin", { hidden: true });
+      location = await page.$(".LocationPin");
+      expect(location).toBe(null);
     }
-    
   });
 
   test("Two location pins show up when both addresses are selected from each box", async () => {
@@ -186,55 +181,50 @@ describe("Map Tests", () => {
     const pickup_box = form_boxes[0];
     const dropoff_box = form_boxes[1];
 
-    box_inputs = ["San Francisco", "San Jose"]
-    for (let id = 0; id < form_boxes.length; id++)
-    {
-      await form_boxes[id].click()
-      await form_boxes[id].type(box_inputs[id])
-      await page.waitForSelector("div[id$=option-0]")
+    box_inputs = ["San Francisco", "San Jose"];
+    for (let id = 0; id < form_boxes.length; id++) {
+      await form_boxes[id].click();
+      await form_boxes[id].type(box_inputs[id]);
+      await page.waitForSelector("div[id$=option-0]");
 
-      let first_option = await page.$("div[id$=option-0]")
-      await first_option.click()
+      let first_option = await page.$("div[id$=option-0]");
+      await first_option.click();
     }
-    await page.waitForTimeout(700)
-    const locations = await page.$$(".LocationPin")
-    expect(locations.length).toBe(2)
-    
-  })
+    await page.waitForTimeout(700);
+    const locations = await page.$$(".LocationPin");
+    expect(locations.length).toBe(2);
+  });
 });
 
 async function outputPrices(box_inputs) {
-  
-  for (const boxInput of box_inputs)
-  {
-    await boxInput.box.click()
-    await boxInput.box.type(boxInput.location)
-    await page.waitForSelector("div[id$=option-0]")
+  for (const boxInput of box_inputs) {
+    await boxInput.box.click();
+    await boxInput.box.type(boxInput.location);
+    await page.waitForSelector("div[id$=option-0]");
 
-    let first_option = await page.$("div[id$=option-0]")
-    await first_option.click()
+    let first_option = await page.$("div[id$=option-0]");
+    await first_option.click();
   }
-    
-    await page.waitForTimeout(8000)
 
-    await page.click(".PricingButton__button");
-} 
+  await page.waitForTimeout(8000);
+
+  await page.click(".PricingButton__button");
+}
 
 describe("PricesOutput tests", () => {
-
   test("display prices when both addresses are filled out and the button is clicked", async () => {
     const form_boxes = await page.$$("input");
     const pickup_box = form_boxes[0];
     const dropoff_box = form_boxes[1];
 
     const box_inputs = [
-      {location: "Santa Cruz", box: pickup_box}, 
-      {location: "Capitola", box: dropoff_box},
-    ]
+      { location: "Santa Cruz", box: pickup_box },
+      { location: "Capitola", box: dropoff_box },
+    ];
 
-    outputPrices(box_inputs)
-    
-    await page.waitForSelector(".PricesOutput")
+    outputPrices(box_inputs);
+
+    await page.waitForSelector(".PricesOutput");
     const prices_display = await page.$(".PricesOutput");
     expect(prices_display).not.toBe(null);
   });
@@ -242,65 +232,62 @@ describe("PricesOutput tests", () => {
   test.skip("prices list does not display on bad request", async () => {
     const form_boxes = await page.$$("input");
     const pickup_box = form_boxes[0];
-    const box_inputs = [
-      {location: "Santa Cruz", box: pickup_box}, 
-    ]
+    const box_inputs = [{ location: "Santa Cruz", box: pickup_box }];
 
-    outputPrices(box_inputs)
-    await page.waitForTimeout(2000)
-    const prices_display = await page.$(".PricesOutput"); 
+    outputPrices(box_inputs);
+    await page.waitForTimeout(2000);
+    const prices_display = await page.$(".PricesOutput");
     expect(prices_display).toBe(null);
   });
 
   test.skip("after one good request, prices list clears after button hit", async () => {
-
     const form_boxes = await page.$$("input");
     const pickup_box = form_boxes[0];
     const dropoff_box = form_boxes[1];
     const box_inputs = [
-      {location: "Santa Cruz", box: pickup_box}, 
-      {location: "Capitola", box: dropoff_box},
-    ]
+      { location: "Santa Cruz", box: pickup_box },
+      { location: "Capitola", box: dropoff_box },
+    ];
 
-      outputPrices(box_inputs)
+    outputPrices(box_inputs);
 
-      await page.waitForSelector(".PricesOutput")
-      let prices_display = await page.$(".PricesOutput");
-      expect(prices_display).not.toBe(null);
+    await page.waitForSelector(".PricesOutput");
+    let prices_display = await page.$(".PricesOutput");
+    expect(prices_display).not.toBe(null);
 
-      await dropoff_box.click()
-      await page.keyboard.press("Delete")
+    await dropoff_box.click();
+    await page.keyboard.press("Delete");
 
-      await page.waitForTimeout(2000)
-      await page.click(".PricingButton__button")
-      await page.waitForTimeout(4000)
+    await page.waitForTimeout(2000);
+    await page.click(".PricingButton__button");
+    await page.waitForTimeout(4000);
 
-      prices_display = await page.$(".PricesOutput");
-      expect(prices_display).toBe(null);
-    });
+    prices_display = await page.$(".PricesOutput");
+    expect(prices_display).toBe(null);
+  });
 
   test.skip("prices list clears after button hit, and displays afterwards", async () => {
-      const form_boxes = await page.$$("input");
-      const pickup_box = form_boxes[0];
-      const dropoff_box = form_boxes[1];
-      const button = await page.$("button");
-      
-      await pickup_box.click();
-      await pickup_box.type("Santa Cruz");
-      await page.keyboard.press("Enter");
+    const form_boxes = await page.$$("input");
+    const pickup_box = form_boxes[0];
+    const dropoff_box = form_boxes[1];
+    const button = await page.$("button");
 
-      await dropoff_box.click();
-      await dropoff_box.type("Capitola");
-      await page.keyboard.press("Enter");
+    await pickup_box.click();
+    await pickup_box.type("Santa Cruz");
+    await page.keyboard.press("Enter");
 
-      await button.click();
+    await dropoff_box.click();
+    await dropoff_box.type("Capitola");
+    await page.keyboard.press("Enter");
 
-      await page.click(".PricingButton__button");
-      let prices_display = await page.$(".PricesOutput");
-      
-      expect(prices_display).toBe(null);
-      prices_display = await page.$(".PricesOutput");
-      
-      expect(prices_display).not.toBe(null);
-    });
+    await button.click();
+
+    await page.click(".PricingButton__button");
+    let prices_display = await page.$(".PricesOutput");
+
+    expect(prices_display).toBe(null);
+    prices_display = await page.$(".PricesOutput");
+
+    expect(prices_display).not.toBe(null);
   });
+});
